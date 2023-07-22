d967858f075cba8918db3353afddf4d917cf939d
David Lay <davidjlay2277@gmail.com>
2023-07-22 16:42:58 -0500

    server running and seeded

---
+++ server/index.js
@@ -0,0 +1,36 @@
+//access environemnt variables
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+// process.env.VAR_NAME  node is providing "process"
+//below, the above is done with deconstruction
+const { SERVER_PORT } = process.env;
+//seed is like another controller
+const { seed } = require("./seed.js");
+
+// pre-built functions
+const {
+  getUserInfo,
+  updateUserInfo,
+  getUserAppt,
+  requestAppointment,
+} = require("./controller.js");
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+// DEV;
+app.post("/seed", seed);
+
+// USER;
+// app.get("/user", getUserInfo);
+// app.put('/user', upd ateUserInfo)
+
+// APPOINTMENTS;
+// app.get("/appt", getUserAppt);
+// app.post("/appt", requestAppointment);
+
+app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`));
